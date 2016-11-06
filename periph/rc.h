62bfbfac77fc8c19f1e870bb22e4f82d4c2d540a
Liam BEGUIN <liambeguin@gmail.com>
2016-11-06 15:05:11 -0500

    radio controller: importing changes
    NOTE: rc_periodical should not be in periph/ bu tin the application!

---
+++ periph/rc.h
@@ -0,0 +1,72 @@
+/*
+ * This file is part of DronolabSTM32-embedded
+ *
+ * DronolabSTM32-embedded is a student project designed
+ * for flying drones.
+ *
+ * Go to < dronolab.etsmtl.ca > to find out more about
+ * the project
+ *
+ *
+ */
+
+/*
+ * rc.h
+ *
+ *  Created on: Nov 15, 2013
+ *      Author: liam <liambeguin.at.gmail.com>
+ */
+
+#ifndef RC_H_
+#define RC_H_
+
+#include <stdbool.h>
+
+//external interrupt
+#include "exti.h"
+#include "stm32f4xx/syscfg.h"
+
+#define RAW_RC_THROTTLE _channel[0].value
+#define RAW_RC_ROLL     _channel[1].value
+#define RAW_RC_PITCH    _channel[2].value
+#define RAW_RC_YAW      _channel[3].value
+
+
+typedef struct {
+	gpio_t		  port;
+	gpio_pin_t 	  pin;
+	syscfg_port_t syscfg_port;
+
+	uint32_t last_time;
+	uint32_t value;
+} channel_t;
+
+
+typedef struct {
+
+	float throttle;
+
+	float roll;
+	float pitch;
+	float yaw;
+
+	bool kill_switch;
+	bool manual_switch;
+
+	bool isAlive;
+
+	bool start_sequence;
+} rc_t;
+
+rc_t drone_radioController;
+
+uint8_t rc_config_channel(timer_t timer, channel_t *channels);
+void rc_print_channel_values();
+// Convert on  [0; 1] scale
+float get_power(uint32_t channel_value);
+// Convert on [-1; 1] scale
+float get_rad  (uint32_t channel_value);
+
+void rc_periodical();
+
+#endif /* RC_H_ */
